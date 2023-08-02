70b2a0b05d4fb857e8479f1a24f73053c95b2c89
Luan Felipe <luanfel2009@gmail.com>
2023-08-02 17:58:49 -0300

    Adição de tabela

--- lista-compras/src/app/mercadorias/mercadorias.component.ts
+++ lista-compras/src/app/mercadorias/mercadorias.component.ts
@@ -3,17 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mercadorias',
   templateUrl: './mercadorias.component.html',
-  styleUrls: ['./mercadorias.component.css']
+  styleUrls: ['./mercadorias.component.css'],
 })
 export class MercadoriasComponent implements OnInit {
-  title = "Lista de Compras";
-  mercadorias = {
-    descricao: "Arroz",
-    quantidade: 1
-  }
-  constructor() { }
+  title = 'Lista de Compras';
+  mercadorias = [
+    {
+      descricao: 'Arroz',
+      quantidade: 1
+    },
+    {
+      descricao: 'Feijão',
+      quantidade: 6
+    },
+    {
+      descricao: 'Óleo',
+      quantidade: 3
+    }
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
